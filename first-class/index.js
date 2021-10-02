e85bbdc3154f29a1aee0cb3043c9cfd140d11a33
jagadeesh <jagadeesh.jeedikanti@gmail.com>
2021-10-02 17:27:53 +0530

    learnt html rendering using javascript

--- first-class/index.js
+++ first-class/index.js
@@ -17,16 +17,81 @@ var students = [
     {
         name:'kiran',
         age:21,
-        isStudent: true
+        isStudent: true,
+        phone:08966674
     },
     {
         name: 'Sai Krishna',
         age:23,
         isStudent: false,
+        phone: 5365643564
+    },
+    {
+        name:'user1',
+        age:16,
+        isStudent: true,
+        phone: 77475756
+    },
+    {
+        name: 'user 2',
+        age:17,
+        isStudent: false,
+        phone: 8764645776
     }
 ] //array type of an object
 
 console.log('1. '+students[0].name +' is '+ students[0].age+' years old')
+
+function printAllStudents(){
+    let mylist = document.getElementById('mylist')
+
+    let mycode = '<caption>All Students</caption>'
+
+    for (let student of students){
+        if(student.isStudent){
+            mycode = mycode+'<li>'+student.name+'</li>'
+        }       
+    }
+    mylist.innerHTML = mycode
+}
+
+function printBelow18(){
+
+    let below = document.getElementById('fasak')
+
+    let htmlstring = '<caption>All students below 18</caption>'
+
+    for (let student of students){
+        if(student.age < 18){
+            htmlstring = htmlstring + '<li>'+ student.name + '</li>'
+            }
+    }
+    if(below){
+        below.innerHTML = htmlstring
+    }
+    
+}
+printBelow18()
+
+function phoneNumber(){
+
+    let phone = document.getElementById('mob')
+
+    let mobile = '<caption>All students phone number</caption>'
+
+    for (let student of students){
+
+        mobile += '<li><strong>' +student.name +'</strong></br>' + 'Phone :' + student.phone +'</br></li>'
+
+    }
+    if(phone){
+        phone.innerHTML = mobile
+    }
+    //phone.innerHTML = mobile
+
+}
+
+// phoneNumber()
 
 //string methods
 //array methods
