6ba64c4c93188935fe1d84c7b3c26afd56291e48
Surya <suryakumar.y@quiddityit.com>
2021-10-02 10:31:28 +0530

    first class

---
+++ first-class/index.js
@@ -0,0 +1,32 @@
+// data types primitive
+const c = 23; //number
+var str = "john doe" //string
+var isStudent = true //boolean
+var s; //undefined
+r = null//null
+
+var student = {
+    name: 'kiran',
+    village:'Gudipadu',
+    isStudent: true,
+    age: 21,
+    qualification: 'Btech'
+} // Object
+
+var students = [
+    {
+        name:'kiran',
+        age:21,
+        isStudent: true
+    },
+    {
+        name: 'Sai Krishna',
+        age:23,
+        isStudent: false,
+    }
+] //array type of an object
+
+console.log('1. '+students[0].name +' is '+ students[0].age+' years old')
+
+//string methods
+//array methods
